3a90104b6958821e4a67801485f6650020594967
Leticia Janet BR <bermudez.leticia.2609@gmail.com>
2025-10-15 16:46:22 -0600

    se agrego script con el modelo JSON

---
+++ scripts/productController.js
@@ -0,0 +1,37 @@
+import {products} from "../scripts/productos.js";
+
+
+// const item = document.getElementById("productInfo");
+
+
+
+// function cargarArticulos(){
+
+//     const data = JSON.parse(products);
+
+
+//     data.map((element) => {
+//         const card = `
+//         <h3 class="product-title">${data.}</h3>
+//         <div class="product-rating">
+//             <div class="stars">
+//                 <i class="bi bi-star-fill"></i>
+//                 <i class="bi bi-star-fill"></i>
+//                 <i class="bi bi-star-fill"></i>
+//                 <i class="bi bi-star-fill"></i>
+//                 <i class="bi bi-star-half"></i>
+//             </div>
+//             <span class="rating-count">(112)</span>
+//         </div>
+//         <div class="product-price">
+//             <span class="current-price">$14,899 MXN</span>
+//             <span class="old-price">$18,999</span>
+//         </div>
+//         <button class="btn btn-custom-primary btn-add-cart">
+//             <i class="bi bi-cart-plus"></i>
+//             Agregar al Carrito
+//         </button>
+//         `;
+        
+//     });
+// }
