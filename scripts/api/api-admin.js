35185c94eeaf152abfb185134d80a5a3ace093ce
Angélica Chin <angie.chin007@gmail.com>
2025-11-12 13:24:57 -0600

    fix: se cambia localhost a la url relativa /api

--- scripts/api/api-admin.js
+++ scripts/api/api-admin.js
@@ -8,7 +8,7 @@
 //  * URL BASE del backend:
 //  */
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = '/api';
 const token = localStorage.getItem('token');
 console.log(token);
 
